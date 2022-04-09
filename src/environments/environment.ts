3d59bf83d27f38e40e1d3fe5d26056e73d30a4bf
EduuVera <103297422+EduuVera@users.noreply.github.com>
2022-04-08 23:03:01 -0400

    MAppViajero
    Commit inicial proyecto MAppViajero

---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebaseConfig: {
+  apiKey: "AIzaSyBoxvebaMHIADjIBM-XU5f965i68U3YEg8",
+  authDomain: "mappviajero.firebaseapp.com",
+  databaseURL: "https://mappviajero-default-rtdb.firebaseio.com",
+  projectId: "mappviajero",
+  storageBucket: "mappviajero.appspot.com",
+  messagingSenderId: "1077794044510",
+  appId: "1:1077794044510:web:b5c42db41ba95e4b9612cf",
+  measurementId: "G-5DHKEM7WRG"
+  },
+
+  //API_URL: 'http://localhost:3000/'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
